160fe5e73c13241ba8eee786bd1528b235a7b77a
CarlosBeiramar <carlosbeiramar@gmail.com>
2023-11-13 00:34:55 +0000

    fix

--- autoload.php
+++ autoload.php
@@ -6,8 +6,8 @@ spl_autoload_register(function ($className) {
     $className = str_replace('\\', DIRECTORY_SEPARATOR, $className);
     $filename = __DIR__ . DIRECTORY_SEPARATOR . $className . '.php';
     if (is_readable($filename)) {
-      require_once($filename);
-    $className = $className[0]==DIRECTORY_SEPARATOR ? $className : DIRECTORY_SEPARATOR.$className;
-  }
+        require_once($filename);
+        $className = $className[0] == DIRECTORY_SEPARATOR ? $className : DIRECTORY_SEPARATOR . $className;
+    }
 });
 require_once("vendor/autoload.php");
