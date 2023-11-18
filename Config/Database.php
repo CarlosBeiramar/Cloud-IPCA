bc7a07cfbf3917e8b32475e46d7af04f8795390d
Jose Alves <alvesjose13@gmail.com>
2023-11-18 00:01:35 +0000

    tests

--- Config/Database.php
+++ Config/Database.php
@@ -16,8 +16,10 @@ class Database
     public static function getConnection()
     {
         if (self::$connection == null) {
-            $dotenv = Dotenv::createImmutable(__DIR__ . '/../');
-            $dotenv->load();
+            if(!isset($_ENV['DB_HOST'])){
+                $dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+                $dotenv->load();
+            }
             self::$connection = new \PDO('mysql:host=' . $_ENV['DB_HOST'] . ';dbname=' . $_ENV['DB_NAME'] . ';charset=utf8', $_ENV['DB_USER'], $_ENV['DB_PASS']);
         }
         return self::$connection;
