95c9a6f2b60e93fd65cc0f117c7f352544f4302e
José Alves <jmtalves@users.noreply.github.com>
2023-11-23 14:40:45 +0000

    Update Database.php

--- Config/Database.php
+++ Config/Database.php
@@ -21,7 +21,8 @@ class Database
             self::$connection = new \PDO('mysql:host=' . $_ENV['DB_HOST'] . ';dbname=' . $_ENV['DB_NAME'] . ';charset=utf8', $_ENV['DB_USER'], $_ENV['DB_PASS']);
             if (!isset($_ENV['ENV_APP']) || $_ENV['ENV_APP'] == "dev") {
                 $sql = "SHOW TABLES FROM " . $_ENV['DB_NAME'];
-                $result = self::$connection->query($sql);
+                $stmt = self::$connection->query($sql);
+                $result = $stmt->fetchAll(\PDO::FETCH_COLUMN);
                 if (!$result || empty($result)) {
                     $sql = @file_get_contents("sql.sql");
                     if ($sql) {
